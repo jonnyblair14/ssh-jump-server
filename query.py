dcbd70c238bd45beaae51f017abf4fece95e9781
jonnyblair14 <jonny@alpha9o1.com>
2026-09-07 18:04:30 +0100

    finished query module

--- query.py
+++ query.py
@@ -1,5 +1,39 @@
+import json
 from pathlib import Path
 
-configPath = "% s/.ssh/config" % Path.home()
-configFile = open(configPath, "a")
-configFile.close()
+
+def main():
+    configPath = "{0}/.ssh/config".format(Path.home())
+    json_str = ""
+    first = True
+    with open(configPath, "rt") as configFile:
+        lines = configFile.readlines()
+
+        for line in lines:
+
+            if line.startswith("Host "):
+                if first != True:
+                    json_str += "}\n{\n"
+                else:
+                    json_str += "{\n"
+                    first = False
+                json_str += '\t"name": '
+            if line.startswith("\tHostName "):
+                json_str += '\t"hostname": '
+            if line.startswith("\tUser "):
+                json_str += '\t"user": '
+            if line.startswith("\tPort "):
+                json_str += '\t"port": '
+            if line.startswith("\tIdentityFile "):
+                json_str += '\t"identity_file": '
+            json_str += '"{0}"'.format(line.split(" ")[1].strip())
+            json_str += "\n"
+        json_str += "}\n"
+
+    configFile.close()
+
+    jsonFile = open("query.json", "w")
+    jsonFile.write(json_str)
+
+
+main()
