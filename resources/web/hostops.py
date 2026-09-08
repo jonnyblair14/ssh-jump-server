66eaff2cb03d28a99b79837afe9b6bcdfc9644ee
jonnyblair14 <jonny@alpha9o1.com>
2026-09-08 11:09:54 +0100

    updated hostops to account for "falsy" values when no input provided in form

--- resources/web/hostops.py
+++ resources/web/hostops.py
@@ -5,17 +5,17 @@ import query
 
 def newhost(hostname, name=None, user=None, port=22, idFile=None):
     temp_dict = {}
-    if name is not None:
+    if name:
         current_host = name
     else:
         current_host = hostname
     temp_dict.update({current_host: {}})
     temp_dict[current_host].update({"hostname": hostname})
-    if user is not None:
+    if user:
         temp_dict[current_host].update({"user": user})
-    if port is not None:
+    if port:
         temp_dict[current_host].update({"port": port})
-    if idFile is not None:
+    if idFile:
         temp_dict[current_host].update({"identity_file": idFile})
 
     query.dict_to_config(temp_dict, "a")
